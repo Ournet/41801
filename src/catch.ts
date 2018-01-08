03aeb58b1c3bbabb08e40287018f99d11fd3fa43
Dumitru Cantea <canteadumitru@gmail.com>
2018-01-08 21:52:56 +0200

    remove console.

--- src/catch.ts
+++ src/catch.ts
@@ -4,7 +4,6 @@ import { maxage } from './maxage';
 import { Request, Response } from "express";
 
 export default function catchError(req: Request, res: Response, error: any) {
-    console.log(error);
     const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
 
     logger.error(error.message || 'errorHandler', {
